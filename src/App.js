40ed9bc1c280945bd4c3d9da17e072487b760872
AniketAtram <anniketatram@gmail.com>
2022-08-27 22:09:37 +0530

    Created card for each name and displayed image

--- src/App.js
+++ src/App.js
@@ -11,57 +11,66 @@ class App extends Component {
     }
   }
   // FETCH DATA FROM API WHENEVR APP IS LOADED
-   componentDidMount(){
+  componentDidMount() {
     this.fetchDataFromAPI();
   }
   // Helper function to fetch data from API 
   fetchDataFromAPI = async () => {
-    try{
+    try {
       let response = await fetch("https://jsonplaceholder.typicode.com/users");
       let data = await response.json();
-      this.setState({monsters:data});
+      this.setState({ monsters: data });
     }
-    catch(error){
+    catch (error) {
       console.error(error.message);
     }
   }
   // Helper function for searching the data
   onSearchHandler = (event) => {
     let { value } = event.target;
-    this.setState({searchString:value});
+    this.setState({ searchString: value });
   }
- 
+
   render() {
-    
-    let {monsters, searchString} = this.state;
+
+    let { monsters, searchString } = this.state;
     let { onSearchHandler } = this;
-    let filteredData = monsters.filter((monster)=>{
+    let filteredData = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase())
     });
-    console.log(filteredData);
+
     return (
       <>
-          <div className='App'>
-          <input 
-            className="search-box" 
-            type="search" 
-            placeholder="Search" 
+        <div className='App'>
+          <input
+            className="search-box"
+            type="search"
+            placeholder="Search"
             aria-label="Search"
             name='searchString'
-            onChange={onSearchHandler} 
+            onChange={onSearchHandler}
           />
-            {
-             filteredData.map(data => {
-                return  <h3 key={data.id}>{data.name}</h3>
-                }
-              )
+          {
+            filteredData.map(data => {
+            
+            return   (<div className="card" style={{ width: "18rem" }} key={data.id}>
+                <img src={`https://robohash.org/${data.id}?set=set2`} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <p className="card-title">{data.name}</p>
+                </div>
+              </div>)
+
             }
-          </div>
-        
+            )
+          }
+
+        </div>
+
       </>
     );
   }
 }
- 
+
 export default App;
 // https://jsonplaceholder.typicode.com/users
+// https://robohash.org/6?set=set2
