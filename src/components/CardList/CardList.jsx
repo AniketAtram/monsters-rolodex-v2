fae1e54bf86da25e3e862483fb91eb6f646179b2
AniketAtram <anniketatram@gmail.com>
2022-08-30 20:00:07 +0530

    Successfully converted each component to functional component

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
 import './CardList.css';
-class CardList extends Component {
-    render() {
-        const { monstersData } = this.props;
-        return (
-            <div className='card-index-container'>
-                {
-                    monstersData.map(data => {
+function CardList(props) {
 
-                        return (
-                            <div className="card" key={data.id}>
-                                <img src={`https://robohash.org/${data.id}?set=set2`} className="card-img-top" alt={data.name} />
-                                <div className="card-body">
-                                    <p className="card-title">{data.name}</p>
-                                </div>
+    const { monstersData } = props;
+    return (
+        <div className='card-index-container'>
+            {
+                monstersData.map(data => {
+
+                    return (
+                        <div className="card" key={data.id}>
+                            <img src={`https://robohash.org/${data.id}?set=set2`} className="card-img-top" alt={data.name} />
+                            <div className="card-body">
+                                <p className="card-title">{data.name}</p>
                             </div>
-                        )
-                    }
+                        </div>
                     )
                 }
-            </div>
-        );
-    }
+                )
+            }
+        </div>
+    );
 }
 
 export default CardList;
